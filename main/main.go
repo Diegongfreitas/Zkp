7d62f054fec7398da188612738c312a8eee5b77b
Diegongfreitas <diegongfreitas@poli.ufrj.br>
2022-05-24 14:56:15 -0300

    Public Witness Serialization Working

--- main/main.go
+++ main/main.go
@@ -1,38 +1,35 @@
 package main
 
 import (
+	
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/frontend/cs/r1cs"
-	//"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"	
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/std/hash/mimc"
 	"github.com/consensys/gnark/backend/witness"
+	
 	"github.com/consensys/gnark-crypto/ecc"
+	
 	"fmt"
 	"bytes"
 	"io/ioutil"
-	"os"
-	//"encoding/json"	
+	"os"	
+
 )
 
-// Circuit defines a pre-image knowledge proof
-// mimc(secret preImage) = public hash
+// Circuit defines a file knowledge proof
+// mimc(secret File) = public hash
 type Circuit struct {
     File_Num frontend.Variable
-    Hash     frontend.Variable `gnark:",public"`
-}
-
-
-var publicWitnessBytes []byte
-var publicWitness *witness.Witness
+    Hash     frontend.Variable `gnark:",public"`}
 
 // Define declares the circuit's constraints
 func (circuit *Circuit) Define(api frontend.API) error {
-    // hash function
-    mimc, _ := mimc.NewMiMC(api)
+
+    mimc, _ := mimc.NewMiMC(api)	
 	mimc.Write(circuit.File_Num)
     // specify constraints
-    // mimc(preImage) == hash
+    // mimc(File_Num) == Hash
     api.AssertIsEqual(circuit.Hash, mimc.Sum())
 
     return nil
@@ -40,11 +37,12 @@ func (circuit *Circuit) Define(api frontend.API) error {
 
 func ttp(){
 	//------------------------------------------------------Generating R1CS---------------------------------------------------------
+	
 	var mimcCircuit Circuit
 	r1cs, _ := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &mimcCircuit)	
 	var r1csbuffer bytes.Buffer
-	_, _ = r1cs.WriteTo(&r1csbuffer)
-	//fmt.Println("r1csbuffer: ", r1csbuffer)	
+	_, _ = r1cs.WriteTo(&r1csbuffer)	
+
 
 	//-------------------------------------------------------Sending R1CS----------------------------------------------------------
 	
@@ -58,11 +56,11 @@ func ttp(){
 	
 	var pkbuffer bytes.Buffer												//Serializing the ProvingKey in order to store it in a file
 	_, _ = pk.WriteTo(&pkbuffer)
-	//fmt.Println("pkbuffer: ", pkbuffer)	
+		
 	
 	var vkbuffer bytes.Buffer												//Serializing the VerifyingKey in order to store it in a file
 	_, _ = vk.WriteTo(&vkbuffer)
-	//fmt.Println("vkbuffer: ", vkbuffer)
+	
 	
 	//-------------------------------------------------------Sending pk-----------------------------------------------------------
 
@@ -78,113 +76,96 @@ func ttp(){
 func prover(){
 	
 	//-------------------------------------------------------Retrieving pk-----------------------------------------------------------
-
+	fmt.Println("\n PROVING \n")
 	pkAsBytes, _ := ioutil.ReadFile("keys/pk.txt")				//Retrieving the serealized ProvingKey from the pk.txt file created by the Third Trusted Party
-	//fmt.Println("pkAsBytes: ", pkAsBytes)
+	
 	var pkbuffer bytes.Buffer 	
 	pkbuffer.Write(pkAsBytes)									//Writing pkbytes into a buffer in order to append it's values in a new groth16.ProvingKey type variable
-	//fmt.Println("pkbuffer: ", pkbuffer)		
+			
 	pk := groth16.NewProvingKey(ecc.BN254)						//Creating new groth16.ProvingKey type variable
 	_, _ = pk.ReadFrom(&pkbuffer)								//Appending pk values from pk.txt file into new groth16.ProvingKey type variable for proof calculation
 	pkbuffer.Reset()
 	
 	//-------------------------------------------------------Retrieving R1CS----------------------------------------------------------
+	
+	r1csAsBytes, _ := ioutil.ReadFile("prover/r1cs.txt")
 
-	r1csAsBytes, _ := ioutil.ReadFile("prover/r1cs.txt")		
-	//fmt.Println("r1csAsBytes: ", r1csAsBytes)
 	var r1csbuffer bytes.Buffer
-	r1csbuffer.Write(r1csAsBytes)
-	//fmt.Println("r1csbuffer: ", r1csbuffer) 
+	r1csbuffer.Write(r1csAsBytes) 
+
 	r1cs := groth16.NewCS(ecc.BN254) 							
 	_, _ = r1cs.ReadFrom(&r1csbuffer)
 	r1csbuffer.Reset()
 
 	//-----------------------------------------------Asking Prover for Witness assignment---------------------------------------------
-
-	fmt.Println("Please Prover, insert hash: ")			
+	
+	fmt.Println("Please Prover, insert file hash: ")			
 	var file_hash string
 	fmt.Scanln(&file_hash)
 
 	assignment := &Circuit{																			//Creating a constraint satisfiable assignment
 		Hash: 		file_hash,																		//both with 77 characters
-		File_Num: "21765111349035677562249794983296132341094",	//witness limit is 32 bytes																							
+		File_Num: "21765111349035677562249794983296132341094",										//witness limit is 32 bytes																							
 	}      
 
 	witness, _ := frontend.NewWitness(assignment, ecc.BN254)	//Codifing the assignment in the form of a Witness using Eliptic Curves(BN254)
-
+	
 	//----------------------------------------------Generating Proof and Public Witness----------------------------------------------------
-	publicWitness, _ = witness.Public()						//Spliting Witness Public Part
-	fmt.Printf("publicWitness: \n", publicWitness)
-	//publicWitnessBytesVector := publicWitness.Vector.MarshalJSON()			//Serializing Witness Public Part in order to send it to the verifier
-	//fmt.Println("publicbytes: \n", publicWitnessBytesVector)
-	publicWitnessBytes, _ = publicWitness.MarshalJSON()
-	fmt.Println("publicWitnessBytes: \n", publicWitnessBytes)
-				
+
+	publicWitness, _ := witness.Public()										//Spliting Witness Public Part
+
+	publicWitnessBytes, _ := publicWitness.MarshalBinary()						//Serializing Witness Public Part in order to send it to the verifier
+	
 	proof, _ := groth16.Prove(r1cs, pk, witness)				
-	fmt.Println("proof: \n", proof)
-	var proofbuffer bytes.Buffer
-	_, _ = proof.WriteTo(&proofbuffer)							
+	
+	var proofbuffer bytes.Buffer												
+	_, _ = proof.WriteTo(&proofbuffer)											//Serializing Proof in order to send it to the verifier
 
 
 	//-------------------------------------------------Sending Proof and Public Witness---------------------------------------------------
 
-	_ = ioutil.WriteFile("prover/proof.txt", proofbuffer.Bytes(), 0666)	//Storing proof into a file that is going to be read by the verifier
+	_ = ioutil.WriteFile("prover/proof.txt", proofbuffer.Bytes(), 0666)			//Storing proof into a file that is going to be read by the verifier
 	proofbuffer.Reset()
-	//_ = ioutil.WriteFile("prover/public_witness.txt", publicWitnessBytes, 0666)	//Storing proof into a file that is going to be read by the verifier
-	//_ = ioutil.WriteFile("prover/public_witness_vector.txt", publicWitnessBytesVector, 0666)
+	_ = ioutil.WriteFile("prover/public_witness.txt", publicWitnessBytes, 0666)	//Storing publicwitness into a file that is going to be read by the verifier
+	
 	
 }
 
 
 
 func verifier(){	
-
-	//--------------------------------------------------------Retrieving vk-----------------------------------------------------------------
 	fmt.Println("\n VERIFYING \n")
+	//--------------------------------------------------------Retrieving vk-----------------------------------------------------------------
+
 	vkAsBytes, _ := ioutil.ReadFile("keys/vk.txt")			//Retrieving the serealized VerifyingKey from the vk.txt file created by the Third Trusted Party
-	//fmt.Println("vkAsBytes: ", vkAsBytes)
+	
 	var vkbuffer bytes.Buffer
-	vkbuffer.Write(vkAsBytes)								//Writing pkbytes into a buffer in order to append it's values in a new groth16.VerifyingKey type variable
-	//fmt.Println("vkbuffer: ", vkbuffer)		
+	vkbuffer.Write(vkAsBytes)								//Writing pkbytes into a buffer in order to append it's values in a new groth16.VerifyingKey type variable		
+	
 	vk := groth16.NewVerifyingKey(ecc.BN254)				//Creating new groth16.VerifyingKey type variable
 	_, _ = vk.ReadFrom(&vkbuffer)							//Appending vk values from vk.txt file into new groth16.VerifyingKey type variable for verification
 	vkbuffer.Reset()
-	//fmt.Println("vk: ", vk)		
+
 
 	//--------------------------------------------------------Retrieving Proof--------------------------------------------------------------
 	proofAsBytes, _ := ioutil.ReadFile("prover/proof.txt")
+	
 	var proofbuffer bytes.Buffer
 	proofbuffer.Write(proofAsBytes)
+	
 	proof := groth16.NewProof(ecc.BN254)					//Creating new groth16.Proof type variable		
 	_, _ = proof.ReadFrom(&proofbuffer)						//Retrieving Proof
 	proofbuffer.Reset()
-	//fmt.Println("proof:", proof)
+
 	
 	//------------------------------------------------------Retrieving Public Witness-------------------------------------------------------
 	
-	//publicWitnessBytes, _ := ioutil.ReadFile("prover/public_witness.txt")
-	//publicWitnessBytesVector, _ := ioutil.ReadFile("prover/public_witness_vector.txt")
-		
-	fmt.Println("publicWitnessbytes:", publicWitnessBytes)
-	//fmt.Println("publicWitnessBytesVector: ", publicWitnessBytesVector)
-	//v, err := *witness.newVector(ecc.BN254)
-	publicWitness.UnmarshalJSON(publicWitnessBytes)
-	fmt.Println("publicWitness:", publicWitness)
-	//json.Unmarshal(publicWitnessBytesVector, &v)
-	
-	/* 
-	publicWitness = &witness.Witness{
-		CurveID: ecc.BN254,
-		Vector:  v,
-	}
-	*/
-	
-	//publicWitness.Vector = 	*witness.Vector(publicWitnessVector)
+	publicWitnessBytes, _ := ioutil.ReadFile("prover/public_witness.txt")	
 
+	publicWitness := &witness.Witness{CurveID: ecc.BN254,} 
 	
-	//fmt.Println("publicvector:", v)
-	
-	
+	publicWitness.UnmarshalBinary(publicWitnessBytes)
+
 	//-------------------------------------------------------------Verifying Proof----------------------------------------------------------
 	err := groth16.Verify(proof, vk, publicWitness)				
 	if err != nil{
